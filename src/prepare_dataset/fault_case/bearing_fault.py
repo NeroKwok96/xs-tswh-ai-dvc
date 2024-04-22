fdebc942dc367ee6f62ea1e44ae0848b2bdaf0e9
NeroKwok96 <nero.kwok@tecky.io>
2024-04-22 17:52:48 +0800

    add python script for create csv with labels

---
+++ src/prepare_dataset/fault_case/bearing_fault.py
@@ -0,0 +1,37 @@
+bearing_fault_case = [
+    {
+        "machine_name": "6B ISO Rm 1 Fan no.2",
+        "machine_type": "Motor",
+        "date_range": ("20231114", "20231120")
+    },
+    {
+        "machine_name": "6A ISO Rm 1 Fan no.1",
+        "machine_type": "Motor",
+        "date_range": ("20230831", "20230906")
+    },
+    {
+        "machine_name": "7A ISO Rm 2 Fan no.2",
+        "machine_type": "Motor",
+        "date_range": ("20230814", "20230821")
+    },
+    {
+        "machine_name": "7A ISO Rm 2 Fan no.2",
+        "machine_type": "Motor",
+        "date_range": ("20231224", "20231228")
+    },
+    {
+        "machine_name": "6B ISO Rm 2 Fan no.1",
+        "machine_type": "Motor",
+        "date_range": ("20240117", "20240202")
+    },
+    {
+        "machine_name": "5A ISO Rm 2 Fan no.1",
+        "machine_type": "Motor",
+        "date_range": ("20231201", "20231231")
+    },
+    {
+        "machine_name": "7A ISO Rm 1 Fan no.1",
+        "machine_type": "Fan-DE",
+        "date_range": ("20230301", "20230430")
+    }
+]
